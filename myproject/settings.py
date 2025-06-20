e30b002d9ebaae47e1d547a74e8d24370c5eb33d
SandeshShewale <sandeshshewale143@gmail.com>
2025-06-20 10:22:55 +0530

    fix: switched to PyMySQL and updated settings for Render deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -105,16 +105,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #     }
 # }
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 import pymysql
 pymysql.install_as_MySQLdb()
